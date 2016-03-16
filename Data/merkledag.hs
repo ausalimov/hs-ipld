02d44d22dd7d3c6b49535b87a065326581b7144c
Alan <ausalimov@gmail.com>
2016-03-16 12:15:46 -0700

    work on traverse

--- Data/merkledag.hs
+++ Data/merkledag.hs
@@ -11,25 +11,63 @@ type IPFS_Simple = M.Map Multihash MDagNode
 -- Note, you will never have two @links in the same level, 
 -- since all nodes are deterministic as per the spec
 
+data SubOb = SubMap { s_map :: M.Map B.ByteString SubOb}
+                 | SubVal { s_val :: B.ByteString} deriving (Show)
+
+
 data MDagNode = MDagNode {
 	n_hash :: Multihash,
-	n_vals :: M.Map B.ByteString B.ByteString
+	n_vals :: SubOb -- will always be the map variation thereof
 } deriving (Show)
 
 
 
 ----- Basic Interface -----
 
-ipld_get_node :: IPFS_Simple -> B.ByteString -> Maybe B.ByteString
-ipld_get_node w t = let b = (B.split '/' t)
-                    in case follow_link w  of 
-		                Just n -> resolve_link w n (tail b) 
+ipld_get_subob :: IPFS_Simple -> B.ByteString -> Maybe SubOb
+ipld_get_subob w t = let b = (B.split '/' t)
+                    in case follow_link w (head b) of 
+		                Just n -> traverse w n (tail b) 
 		                Nothing -> Nothing
 
+traverse :: IPFS_Simple -> MDagNode -> [B.ByteString] -> Maybe SubOb
+traverse w node [] = Just (n_vals node)
+traverse w node b = case (n_vals node) of 
+	SubMap node_map -> case M.lookup (head b) node_map of 
+		Just subob -> case subob of 
+			SubVal _ -> Just subob
+			SubMap _ -> resolve_in_node w subob (tail b)
+		Nothing -> resolve_link w (n_vals node) (tail b)
+				
 
--- Given a node and a remaining path traversal, return the node at the end of the line
--- If the head of the traversal is not in the vals, return a failure (Nothing)
-resolve_link :: IPFS_Simple -> MDagNode -> [B.ByteString] -> Maybe B.ByteString
+resolve_in_node :: IPFS_Simple -> 
+				   SubOb -> 
+				   [B.ByteString] -> 
+				   Maybe SubOb
+resolve_in_node w s [] = Just s 
+resolve_in_node w s b = case s of 
+	SubVal _ -> Just s 
+	SubMap m -> case M.lookup (head b) m of 
+		Just subob -> resolve_in_node w subob (tail b) 
+		Nothing -> case M.lookup "@link" node_map of 
+			Nothing -> Nothing 
+			Just l -> case l of 
+				SubVal link -> case 
+			
+
+resolve_link :: IPFS_Simple -> SubOb -> [B.ByteString] -> Maybe SubOb
+resolve_link w s b = case s of 
+	SubMap node_map -> case M.lookup "@link" node_map of 
+			Nothing -> Nothing 
+			Just l -> case l of 
+				SubVal link -> case link == (head b) of 
+					False -> Nothing
+					True -> case follow_link w link of 
+						Just node' -> traverse w node' (tail b)
+						Nothing -> Nothing
+	
+
+{-
 resolve_link w n [b] = M.lookup b (n_vals n) -- last one won't be link
 resolve_link w n (b:bs) = case M.lookup b (n_vals n) of	
 	Just n' -> resolve_link w n' bs -- it's not a link
@@ -39,6 +77,7 @@ resolve_link w n (b:bs) = case M.lookup b (n_vals n) of
 follow_link w b of 
 		Nothing -> Nothing -- Hash doesn't exist! 
 		Just new_node -> resolve_link w new_node bs
+-}
 
 
 
